776c021574b8ff7c2a2053dffb30d65fecfc3ac8
andriagulbiani <167685576+andriagulbiani@users.noreply.github.com>
2024-06-30 06:23:27 -0700

    updated phones page

--- js/main.js
+++ js/main.js
@@ -10,8 +10,40 @@ window.onload = function () {
 	const menu_btn = document.querySelector('.hamburger');
 	const mobile_menu = document.querySelector('.mobile-nav');
 
+
 	menu_btn.addEventListener('click', function () {
 		menu_btn.classList.toggle('is-active');
 		mobile_menu.classList.toggle('is-active');
+		price_btn.classList.toggle('is-active');
 	});
+
+	document.querySelectorAll('.price-btn').forEach((btn) => {
+		btn.addEventListener('click', () => {
+		  document.querySelectorAll('.price-btn').forEach((btn) => btn.classList.remove('clicked'));
+		  btn.classList.add('clicked');
+		});
+	  });
+
+	// const price_btn = document.querySelector('.price-btn');
+
+
+	// price_btn.addEventListener('click', function () {
+	// 	price_btn.classList.toggle('is-active');
+	// });
+
+	// const price_btn2 = document.querySelector('.price-btn2'); 
+
+
+	// price_btn2.addEventListener('click', function () {
+	// 	price_btn2.classList.toggle('is-active');
+	// });
+
+	// const price_btn3 = document.querySelector('.price-btn3');
+
+
+	// price_btn3.addEventListener('click', function () {
+	// 	price_btn3.classList.toggle('is-active');
+	// });
+
+
 }
