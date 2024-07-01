6cfde549c3012b86ff5163bcd27b42a58a22b738
andriagulbiani <167685576+andriagulbiani@users.noreply.github.com>
2024-07-01 11:44:18 -0700

    updated burger menu

--- js/main.js
+++ js/main.js
@@ -9,12 +9,13 @@ window.onload = function () {
 
 	const menu_btn = document.querySelector('.hamburger');
 	const mobile_menu = document.querySelector('.mobile-nav');
+	const header_btn = document.querySelector('.header')
 
 
 	menu_btn.addEventListener('click', function () {
 		menu_btn.classList.toggle('is-active');
 		mobile_menu.classList.toggle('is-active');
-		price_btn.classList.toggle('is-active');
+		header_btn.classList.toggle('is-active');
 	});
 
 	document.querySelectorAll('.price-btn').forEach((btn) => {
@@ -23,27 +24,5 @@ window.onload = function () {
 		  btn.classList.add('clicked');
 		});
 	  });
-
-	// const price_btn = document.querySelector('.price-btn');
-
-
-	// price_btn.addEventListener('click', function () {
-	// 	price_btn.classList.toggle('is-active');
-	// });
-
-	// const price_btn2 = document.querySelector('.price-btn2'); 
-
-
-	// price_btn2.addEventListener('click', function () {
-	// 	price_btn2.classList.toggle('is-active');
-	// });
-
-	// const price_btn3 = document.querySelector('.price-btn3');
-
-
-	// price_btn3.addEventListener('click', function () {
-	// 	price_btn3.classList.toggle('is-active');
-	// });
-
 
 }
